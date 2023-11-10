e4fc8a79820c3d1c5c1687c96c4b65a54438d4ca
Ayomide24500 <Ayomideadisa83@gmail.com>
2023-11-10 15:27:26 +0100

    second

--- index.ts
+++ index.ts
@@ -7,3 +7,15 @@ const port: number = 2000;
 const server = app.listen(port, () => {
   console.log("server is up");
 });
+
+process.on("uncaughtException", (err: Error) => {
+  console.log("uncaughtException", err);
+  process.exit(1);
+});
+process.on("rejectionHandled", (reason: any) => {
+  console.log("rejectionHandled", reason);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
